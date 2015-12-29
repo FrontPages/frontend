46b2373355a17763983577d37b8b3e0c560a52b8
Jesse Pinho <jesse@jessepinho.com>
2015-12-28 21:14:35 -0600

    Mock headlines and style them

--- server/mocks/headlines.js
+++ server/mocks/headlines.js
@@ -1,15 +1,21 @@
+var loremIpsum = require('lorem-ipsum');
+
 /*jshint node:true*/
 module.exports = function(app) {
   var express = require('express'),
+      headlines = [],
       router = express.Router(),
-      headlines = [
-        {
-          id: 1,
-          snapshot: 'some-url',
-          title: 'Some headline title',
-          url: 'http://nytimes.com'
-        }
-      ];
+      title;
+
+  for (var id = 1; id <= 20; id++) {
+    title = loremIpsum({ units: 'words', count: 10 });
+    headlines.push({
+      id: id,
+      snapshot: `http://lorempixel.com/g/209/250/business/${id % 10}`,
+      title: title,
+      url: 'http://nytimes.com'
+    });
+  }
 
   router.get('/', function(req, res) {
     res.send({
