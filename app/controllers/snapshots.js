69647ed3c9776ae31adee4f140e3653b537a627a
Jesse Pinho <jesse@jessepinho.com>
2016-01-03 15:46:55 -0600

    Add properties for sites

--- app/controllers/snapshots.js
+++ app/controllers/snapshots.js
@@ -2,5 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   snapshotSorting: ['created_at:desc'],
-  sortedSnapshots: Ember.computed.sort('model', 'snapshotSorting')
+  siteSorting: ['name'],
+  sortedSites: Ember.computed.sort('sites', 'siteSorting'),
+  sortedSnapshots: Ember.computed.sort('model', 'snapshotSorting'),
+
+  sites: Ember.computed(function() {
+    return this.store.findAll('site');
+  })
 });
