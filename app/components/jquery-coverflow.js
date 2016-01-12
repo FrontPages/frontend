7834b009166708e5c86afdf4006fc229fa715a03
Jesse Pinho <jesse@jessepinho.com>
2016-01-12 09:21:31 -0600

    Refactor coverflow component to use options

--- app/components/jquery-coverflow.js
+++ app/components/jquery-coverflow.js
@@ -2,23 +2,45 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   didRender() {
-    var coverflow = Ember.$(this.get('element')).coverflow({
-      animateStep: this.get('animateStep'),
-      density: 1,
-      enableWheel: false,
-      innerAngle: 0,
-      innerScale: 0.95,
-      outerAngle: 0,
-      outerScale: 0.1,
-    });
-
+    var coverflow = Ember.$(this.get('element')).coverflow(this.get('_options'));
     this.set('coverflow', coverflow);
   },
 
-  refresh: Ember.observer('observeForRefresh', function() {
+  _refresh: Ember.observer('observeForRefresh', function() {
     Ember.run.next(() => {
       this.get('coverflow').coverflow('index', 0);
       this.get('coverflow').coverflow('refresh');
     });
+  }),
+
+  _optionList: ['animateStep',
+                'animateComplete',
+                'change',
+                'confirm',
+                'density',
+                'duration',
+                'easing',
+                'enableClick',
+                'enableKeyboard',
+                'enableWheel',
+                'index',
+                'innerAngle',
+                'innerCss',
+                'innerOffset',
+                'innerScale',
+                'outerAngle',
+                'outerCss',
+                'outerOffset',
+                'outerScale',
+                'select'],
+
+  _options: Ember.computed('_optionList.[]', function() {
+    var options = {};
+    this.get('_optionList').forEach((option) => {
+      if (Ember.typeOf(this.get(option)) !== 'undefined') {
+        options[option] = this.get(option);
+      }
+    });
+    return options;
   })
 });
