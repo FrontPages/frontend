8a9b4cac66edf4934a315d6bc27d4f28fbdddebf
Jesse Pinho <jesse@jessepinho.com>
2016-01-11 20:45:23 -0600

    Show loading GIF until image is loaded; set image width

--- app/controllers/sites/site.js
+++ app/controllers/sites/site.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  // Empty GIF courtesy of http://stackoverflow.com/a/14115340/974981
+  emptyGIF: "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=",
+
   snapshots: Ember.computed(function() {
     return this.store.findAll('snapshot');
   }),
@@ -9,5 +12,15 @@ export default Ember.Controller.extend({
     return this.get('snapshots').filter((snapshot) => {
       return snapshot.get('site.id') === this.get('model.id');
     });
-  })
+  }),
+
+  attachImageLoadListener: Ember.observer('filteredSnapshots.[]', function() {
+    Ember.run.next(() => {
+      Ember.$('.site-screenshot-image img').load(function() {
+        Ember.$(this).parent().removeClass('site-screenshot-image-loading');
+      }).each(function() {
+        Ember.$(this).attr('src', Ember.$(this).data('src'));
+      });
+    });
+  }),
 });
