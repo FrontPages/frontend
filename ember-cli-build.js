ce12025b0a7a0b6de9157f380d7db6a2cedfaa2d
Jesse Pinho <jesse@jessepinho.com>
2016-01-19 07:54:30 -0600

    Remove unnecessary imports

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -23,10 +23,6 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
   app.import('bower_components/material-design-lite/material.js');
-  app.import('bower_components/jquery-ui/jquery-ui.js');
-  app.import('bower_components/coverflow/jquery.coverflow.js');
-  app.import('bower_components/coverflow/jquery.interpolate.js');
-  app.import('bower_components/coverflow/jquery.touchSwipe.min.js');
   app.import('bower_components/xcolor/jquery.xcolor.js');
 
   return app.toTree();
