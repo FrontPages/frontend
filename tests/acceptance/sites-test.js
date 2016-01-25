9b4b0e19359ed25b13a03e6c502ab8945a6cb877
Jesse Pinho <jesse@jessepinho.com>
2016-01-24 21:29:24 -0600

    Add test for links to each site

--- tests/acceptance/sites-test.js
+++ tests/acceptance/sites-test.js
@@ -6,7 +6,12 @@ moduleForAcceptance('Acceptance | sites', {
   beforeEach() {
     this.server = new Pretender(function() {
       this.get('/sites', function() {
-        var mockJSON = { sites: [{ id: 1, name: 'Site Name' }] };
+        var mockJSON = {
+          sites: [
+            { id: 1, name: 'Site 1' },
+            { id: 2, name: 'Site 2' }
+          ]
+        };
         return [200, {}, JSON.stringify(mockJSON)];
       });
 
@@ -26,10 +31,19 @@ moduleForAcceptance('Acceptance | sites', {
   }
 });
 
-test('redirects to the first site', function(assert) {
+test('it redirects to the first site', function(assert) {
   visit('/');
 
   andThen(function() {
-    assert.equal(currentURL(), '/site/site-name');
+    assert.equal(currentURL(), '/site/site-1');
+  });
+});
+
+test('it has links to each site', function(assert) {
+  visit('/');
+
+  andThen(() => {
+    assert.equal(find('.mdl-layout__tab-bar a:eq(0) .sites-name').text(), 'Site 1');
+    assert.equal(find('.mdl-layout__tab-bar a:eq(1) .sites-name').text(), 'Site 2');
   });
 });
