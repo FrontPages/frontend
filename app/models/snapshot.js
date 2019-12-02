e9a9f56cd8a4b9ddc7e88a95b1b029780aff9458
Jesse Pinho <jesse@jessepinho.com>
2019-12-02 19:45:11 +0100

    Fix the property name in the model

--- app/models/snapshot.js
+++ app/models/snapshot.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  file_path: DS.attr('string'),
+  filename_s3: DS.attr('string'),
   headlines: DS.hasMany('headline'),
   site: DS.belongsTo('site'),
   created_at: DS.attr('date')
